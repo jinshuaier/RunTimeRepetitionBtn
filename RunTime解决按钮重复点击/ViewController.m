60ee648fae9efc2950aa98f03a57c8a213a915e4
jinshuaier <470401910@qq.com>
2017-09-18 14:14:16 +0800

    first commit

--- RunTime解决按钮重复点击/ViewController.m
+++ RunTime解决按钮重复点击/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIControl+recurClick.h"
 @interface ViewController ()
 
 @end
@@ -16,9 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *btn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    btn.frame = CGRectMake(100, 100, 100, 100);
+    [btn setTitle:@"点击" forState:(UIControlStateNormal)];
+    btn.backgroundColor = [UIColor blackColor];
+    [btn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+    [btn addTarget:self action:@selector(click:) forControlEvents:(UIControlEventTouchUpInside)];
+    btn.uxy_acceptEventInterval = 2;
+    [self.view addSubview:btn];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark -- 点击事件
+- (void)click:(UIButton *)sender
+{
+    NSLog(@"haha");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
